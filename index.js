26b325c634e94c9e852724476ac433a0163b7fb0
d7o1d1s0 <d7sarmy@gmail.com>
2022-07-04 12:05:20 +0100

    neatening up

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-//1. Import coingecko-api
 const CoinGecko = require('coingecko-api');
-var Bottleneck = require("bottleneck/es5");
-
-
+const Bottleneck = require("bottleneck/es5");
 
 const coinGeckoAPI = new Bottleneck({
     maxConcurrent: 1,
     minTime: 400
 });
 
-//2. Initiate the CoinGecko API Client
 const CoinGeckoClient = new CoinGecko();
-
-//3. Make calls
 
 const activeTokensDetails = [
     {
@@ -28,7 +22,6 @@ const activeTokensDetails = [
         "BUSD": {
             "id": 6,
             "address": "0x4fabb145d64652a948d72533023f6e7a623c7c53",
-            "polygon": "0xdAb529f40E671A1D4bF91361c21bf9f0C9712ab7",
             "symbol": "BUSD",
             "decimals": 18,
             "enabledForFees": true
@@ -38,8 +31,6 @@ const activeTokensDetails = [
         "DAI": {
             "id": 1,
             "address": "0x6b175474e89094c44da98b954eedeac495271d0f",
-            "polygon": "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063",
-            "arbitrum": "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1",
             "symbol": "DAI",
             "decimals": 18,
             "enabledForFees": true
@@ -85,8 +76,6 @@ const activeTokensDetails = [
         "LINK": {
             "id": 10,
             "address": "0x514910771af9ca656af840dff83e8264ecf986ca",
-            "polygon": "0x53e0bca35ec356bd5dddfebbd1fc0fd03fabad39",
-            "arbitrum": "0xf97f4df75117a78c1a5a0dbb814af92458539fb4",
             "symbol": "LINK",
             "decimals": 18,
             "enabledForFees": true
@@ -96,7 +85,6 @@ const activeTokensDetails = [
         "MATIC": {
             "id": 149,
             "address": "0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0",
-            "polygon": "0x0000000000000000000000000000000000001010",
             "symbol": "MATIC",
             "decimals": 18,
             "enabledForFees": true
@@ -142,8 +130,6 @@ const activeTokensDetails = [
         "USDC": {
             "id": 2,
             "address": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-            "polygon": "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
-            "arbitrum": "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8",
             "symbol": "USDC",
             "decimals": 6,
             "enabledForFees": true
@@ -153,8 +139,6 @@ const activeTokensDetails = [
         "USDT": {
             "id": 4,
             "address": "0xdac17f958d2ee523a2206206994597c13d831ec7",
-            "polygon": "0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
-            "arbitrum": "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9",
             "symbol": "USDT",
             "decimals": 6,
             "enabledForFees": true
@@ -164,8 +148,6 @@ const activeTokensDetails = [
         "WBTC": {
             "id": 15,
             "address": "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
-            "polygon": "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6",
-            "arbitrum": "0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f",
             "symbol": "WBTC",
             "decimals": 8,
             "enabledForFees": true
@@ -211,16 +193,13 @@ const getRelevantTokens = async function () {
                     }
                 }
             }
-            // console.log(idList)
             getTokenDetails(idList, tokenDetails)
         })
 }
 
 getRelevantTokens()
 
-
 const getTokenDetails = async function (list, tokenDetails) {
-    let fullTokenDetails = []
     for (let i = 0; i < list.length; i++) {
         let id = list[i].id
         coinGeckoAPI.schedule(() => {
@@ -252,7 +231,5 @@ const getTokenDetails = async function (list, tokenDetails) {
                 })
         })
     }
-
-    // console.log(fullTokenDetails)
 }
 
